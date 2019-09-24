489c6b12840749fbb9466f0f11e312297f0216e7
mahdi gueffaz <macbook@MacBook-Pro-de-macbook.local>
2019-09-24 22:42:08 +0200

    Add bundle make + doctrine

---
+++ app/src/Entity/Plane.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\PlaneRepository")
+ */
+class Plane
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $immatriculation;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $year;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getImmatriculation(): ?string
+    {
+        return $this->immatriculation;
+    }
+
+    public function setImmatriculation(?string $immatriculation): self
+    {
+        $this->immatriculation = $immatriculation;
+
+        return $this;
+    }
+
+    public function getYear(): ?string
+    {
+        return $this->Year;
+    }
+
+    public function setYear(string $Year): self
+    {
+        $this->Year = $Year;
+
+        return $this;
+    }
+}
