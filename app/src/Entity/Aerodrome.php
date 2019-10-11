e4e510112b968a8c7a03fa70e020bb89a092ef6d
mahdi gueffaz <macbook@MacBook-Pro-de-macbook.local>
2019-10-12 01:03:08 +0200

    Add aerodrome entity

---
+++ app/src/Entity/Aerodrome.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ApiResource
+ * @ORM\Entity
+ */
+class Aerodrome
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $code;
+
+}
